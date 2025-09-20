0cbf6370c5706e8119757ff945488fa293ce9179
Jefz Aljabbar <119145812+JefzAljabbar@users.noreply.github.com>
2025-09-21 03:11:47 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,8 +1,8 @@
 import { connect } from "cloudflare:sockets";
 
 // Variables
-let serviceName = "";
-let APP_DOMAIN = "";
+let serviceName = "jefza70.workers.dev";
+let APP_DOMAIN = "flexibilitymoment";
 
 let prxIP = "";
 let cachedPrxList = [];
